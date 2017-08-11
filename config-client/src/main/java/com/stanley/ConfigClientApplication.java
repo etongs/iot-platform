2f2923965b8653223de21c612f9b69f2a517b5cc
etongs <etongs@163.com>
2017-08-11 12:09:23 +0800

    config测试

--- config-client/src/main/java/com/stanley/ConfigClientApplication.java
+++ config-client/src/main/java/com/stanley/ConfigClientApplication.java
@@ -3,11 +3,13 @@ package com.stanley;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
@@ -15,12 +17,12 @@ public class ConfigClientApplication {
 	}
 
 
-	@Value("${test.c-ribbon.my-value}")
-	private String val;
+	@Value("${mybatis.aliase-package}")
+	private boolean val;
 
 	@RequestMapping("/hi")
 	public String hi(){
-		return val;
+		return String.valueOf(val);
 	}
 
 
