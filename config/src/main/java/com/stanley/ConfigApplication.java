c44f2758b9c3e4a55a705b21d7dfeab989f6dd3e
etongs <etongs@163.com>
2017-08-18 10:28:50 +0800

    config server配置修改，改成本地读取

--- config/src/main/java/com/stanley/ConfigApplication.java
+++ config/src/main/java/com/stanley/ConfigApplication.java
@@ -1,17 +1,29 @@
 package com.stanley;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * Spring Cloud Config 配置服务器
+ * @date 2017/8/15
+ * @author 13346450@qq.com 童晟
+ */
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
 public class ConfigApplication {
 
+	/**
+	 * @Description
+	 * 需要配置profile为native才能读取本地配置
+	 * @date 2017/8/15
+	 * @author 13346450@qq.com 童晟
+	 * @param
+	 * @return
+	 */
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigApplication.class, args);
+		new SpringApplicationBuilder(ConfigApplication.class).web(true).profiles("native").run(args);
 	}
 }
